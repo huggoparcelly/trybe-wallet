c2d2e553d22e088c290a29c3cc10b1501041e939
Huggo Parcelly <h.parcelly@gmail.com>
2021-10-04 18:14:24 -0300

    Updates

---
+++ precache-manifest.88e350bb5a66426fe3e1c9e585943f62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f110d46741391d289a2f6f88d1f7309",
+    "url": "/trybe-wallet/index.html"
+  },
+  {
+    "revision": "25ac093c399fe0343e6e",
+    "url": "/trybe-wallet/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "55e2f0bbc74d1aa6fb16",
+    "url": "/trybe-wallet/static/js/2.87edbcdb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybe-wallet/static/js/2.87edbcdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25ac093c399fe0343e6e",
+    "url": "/trybe-wallet/static/js/main.57970cce.chunk.js"
+  },
+  {
+    "revision": "59d3c1ec903278203d08",
+    "url": "/trybe-wallet/static/js/runtime-main.2e2bea36.js"
+  }
+]);
